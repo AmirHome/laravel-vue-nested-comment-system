5a6f6e6a87b151596d8d033f1b4c6e5fe33e2b17
amirhome <amir.email@gmail.com>
2021-11-25 07:11:14 +0300

    Unit Test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -5,17 +5,62 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+use App\Models\Comment;
 
+class CommentTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function test_index_page_load()
+    {
+        $this->withoutExceptionHandling();
+
+        $response = $this->get('/');
         $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function test_comment_create() {
+        $this->withoutExceptionHandling();
+        $response = $this->post('/api/comments', [
+            'name' => 'Amir Hosseinzadeh',
+            'comment' => 'Test Unit Comment'
+        ]);
+
+        $response->assertOk();
+        $this->assertCount(1, Comment::all());
+    }
+
+    /** @test */
+    public function test_comment_form_validation() {
+        // $this->withoutExceptionHandling();
+
+        $response = $this->post('/api/comments', [
+            'name' => '',
+            'comment' => 'Test Unit Comment'
+        ]);
+
+        $response->assertSessionHasErrors('name');
+
+        $response = $this->post('/api/comments', [
+            'name' => 'Amir Hosseinzadeh',
+            'comment' => ''
+        ]);
+
+        $response->assertSessionHasErrors('comment');
+    }
+
+    /** @test */
+    public function test_comment_listing_returns() {
+        $this->test_comment_create();
+
+        $response = $this->get('/api/comments');
+
+        $response->assertOk();
+
+        $this->assertDatabaseHas('comments', [
+            'name' => 'Amir Hosseinzadeh'
+        ]);
     }
 }
